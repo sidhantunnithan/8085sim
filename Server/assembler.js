ca851dfccddcb818a02e143671ee39af96c9ebfc
sreerajpr06 <sreerajpr06@gmail.com>
2020-12-23 19:27:27 +0530

    updated assembler.js

--- Server/assembler.js
+++ Server/assembler.js
@@ -45,7 +45,7 @@ function getOpcodes(instructions){
     let opcodeList = [];
     try{
         for(let i=0; i<instructions.length; i++){
-            if(checkInstructionError(instructions[i]))
+            if(checkInstructionError(instructions[i])[0])
                 throw("Invalid Instruction: " + instructions[i]);
 
             let code = parse(instructions[i]);
@@ -60,17 +60,17 @@ function getOpcodes(instructions){
     }
 }
 
-// pgm = "LXI H 5000 MOV A M MOV B A MVI C 09 ADD B DCR C JNZ 5200 INX H ADD M STA 5100 HLT"
-// instructionList = pgm.split(' ');
-// console.log(pgm);
-// console.log(instructionList);
+pgm = "LXI H 5000 MOV A M MOV B A MVI C 09 ADD B DCR C JNZ 5200 INX H ADD M STA 5100 HLT"
+instructionList = pgm.split(' ');
+console.log(pgm);
+console.log(instructionList);
 
-// // instructionList = ["MOV","A","B","MOV","C","D","ADD","B","ADI","05", "STA","5020"];
+// instructionList = ["MOV","A","B","MOV","C","D","ADD","B","ADI","05", "STA","5020"];
 
-// instructions = getInstructions(instructionList);
+instructions = getInstructions(instructionList);
 
-// console.log(instructions);
-// // for(let i=0;i<instructions.length;i++){
-// //     getOpcodes()
-// // }
-// console.log(getOpcodes(instructions));
+console.log(instructions);
+// for(let i=0;i<instructions.length;i++){
+//     getOpcodes()
+// }
+console.log(getOpcodes(instructions));
