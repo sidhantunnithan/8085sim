75789847e0a96ebd495be440c5cd51d2193dc3fa
sreerajpr06 <sreerajpr06@gmail.com>
2020-12-26 18:46:13 +0530

    removed unused data structures

--- Server/Resources/dataStructures.js
+++ Server/Resources/dataStructures.js
@@ -1,19 +1,5 @@
 const fs = require('fs');
 
-// contains register values in hex
-var  registers = {
-    'A' : '00',
-    'B' : '00',
-    'C' : '00',
-    'D' : '00',
-    'E' : '00',
-    'H' : '00',
-    'L' : '00'
-};  
-
-// contains label -> memory location
-var label = {};
-
 // contains mnemonic -> number of bytes
 var numBytes = {};
 
@@ -32,4 +18,3 @@ Object.entries(instructions).forEach(([key, value]) => {
 })
 
 module.exports = {numBytes, opcode};
-module.exports = {registers, numBytes, opcode};
