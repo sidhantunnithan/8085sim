030b503546bb4695e4d882c95761f15676412767
sreerajpr06 <sreerajpr06@gmail.com>
2020-12-26 18:44:23 +0530

    added comments

--- Server/Resources/parse.js
+++ Server/Resources/parse.js
@@ -1,7 +1,4 @@
-/////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////
-
-const { registers, numBytes, opcode} = require(__dirname + '/dataStructures.js');
+const {numBytes, opcode} = require(__dirname + '/dataStructures.js');
 
 const errorDict = {
     0 : "No Error",
@@ -11,19 +8,13 @@ const errorDict = {
     4 : "Unknown error"
 };
 
-/////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////
 
-// to print all register contents
-function showAllRegContents(){
-    for(let reg in registers){
-        console.log(registers[reg]);
-    }
-}
-
-
-// to check if the given data is a hexidecimal or not
 function isHex(operand){
+    /*  
+        Checks if the given data is a hexidecimal or not.
+        Returns true if the data is hexadecimal, else returns false.
+    */ 
+
     let isHex = true;
     let len = operand.length;
 
@@ -37,45 +28,41 @@ function isHex(operand){
 }
 
 
-/* 
-    Checks whether a given instruction is valid or not.
-    Returns false if the instruction is valid, else returns true
-*/
 function checkInstructionError(instruction, label){
+    /* 
+        Checks whether a given instruction is valid or not.
+        Returns false if the instruction is valid, else returns true.
+    */
+
     let isError = false;
     let errorCode = 0;
 
     try{
 
-        // for labels
-        if(instruction in label){
+        if(instruction in label){           // for labels
             return [isError, errorCode];
         }
 
-        // for 1 byte instructions
-        if (instruction in opcode){
+        if (instruction in opcode){         // for 1 byte instructions
             return [isError, errorCode];
         }
 
         else{
             let mnemonic = (instruction.split(' '))[0];
 
-            // for 2 byte instructions like ADI and 3 byte instructions like STA
-            if (mnemonic in opcode){
+            if (mnemonic in opcode){        // for 2 byte instructions like ADI and 3 byte instructions like STA
                 let operand = (instruction.split(' '))[1];
                 let instructionSize = numBytes[mnemonic];
-                // for 2 byte instructions like ADI
-                if (instructionSize == 2){
-                    // to check if the operand is of 1 byte size
-                    if (operand.length != 2 || !isHex(operand)){
+                
+                if (instructionSize == 2){                          // for 2 byte instructions like ADI
+                    if (operand.length != 2 || !isHex(operand)){    // to check if the operand is of 1 byte size
                         isError = true;
                         errorCode = 2;
                     }
 
                 }
 
-                // for 3 byte instructions like STA
-                else if (instructionSize == 3){
+                else if (instructionSize == 3){                     // for 3 byte instructions like STA
                     if ((operand.length != 4 || !isHex(operand)) && !((operand + ":") in label)){
                         isError = true;
                         errorCode = 2;
@@ -86,8 +73,7 @@ function checkInstructionError(instruction, label){
                 return [isError, errorCode];
             }
 
-            // for 2 byte instructions like MVI A and 3 byte instructions like LXI H
-            else{
+            else{                           // for 2 byte instructions like MVI A and 3 byte instructions like LXI H
                 mnemonic = mnemonic + " " + instruction.split(' ')[1];
 
                 if (mnemonic in opcode){
@@ -128,9 +114,13 @@ function checkInstructionError(instruction, label){
 }
 
 
-// to parse the instruction to object code
-
 function parse(instruction, label){
+    /* 
+        Parses the instruction to object code.
+        Returns the object code of the instruction if the instruction is valid.
+        Else returns error.
+    */
+
     try{
         let err = checkInstructionError(instruction, label);
 
@@ -144,26 +134,22 @@ function parse(instruction, label){
             return;
         }
 
-        // for 1 byte instructions
-        if (instruction in opcode){
+        if (instruction in opcode){             // for 1 byte instructions
             code = [opcode[instruction]];
 
         }
 
-        // for all 2 & 3 byte instructions
-        else{
+        else{                                   // for all 2 & 3 byte instructions
             let mnemonic = instruction.split(' ')[0];
 
-            // for 2 byte instructions like ADI and 3 byte instructions like STA
-            if(mnemonic in numBytes){
+            if(mnemonic in numBytes){           // for 2 byte instructions like ADI and 3 byte instructions like STA
                 instructionSize = numBytes[mnemonic];
 
-                // for 2 byte instructions like ADI
-                if(instructionSize == 2){
+                if(instructionSize == 2){       // for 2 byte instructions like ADI
                     code = [opcode[mnemonic], instruction.split(' ')[1]];
                 }
-                // for 3 byte instructions like STA
-                else if(instructionSize == 3){
+    
+                else if(instructionSize == 3){  // for 3 byte instructions like STA
                     operand = instruction.split(' ')[1];
                     if((operand + ":") in label)
                         operand = String(label[operand + ":"]);
@@ -171,17 +157,15 @@ function parse(instruction, label){
                     code = [opcode[mnemonic], operand.slice(2), operand.slice(0,2)];
                 }
             }
-            // for 2 byte instructions like MVI A and 3 byte instructions like LXI H
-            else{
+
+            else{                               // for 2 byte instructions like MVI A and 3 byte instructions like LXI H
                 mnemonic = mnemonic + " " + instruction.split(' ')[1];
                 instructionSize = numBytes[mnemonic];
 
-                // for 2 byte instructions like MVI A
-                if(instructionSize == 2){
+                if(instructionSize == 2){       // for 2 byte instructions like MVI A
                     code = [opcode[mnemonic], instruction.split(' ')[2]];
                 }
-                // for 3 byte instructions like LXI H
-                else{
+                else{                           // for 3 byte instructions like LXI H
                     operand = instruction.split(' ')[2];
                     if((operand + ":") in label)
                         operand = String(label[(operand + ":")]);
@@ -199,42 +183,3 @@ function parse(instruction, label){
 }
 
 module.exports = {checkInstructionError, parse};
-
-
-
-// console.log(checkInstructionError("MOV A B"));
-// console.log(checkInstructionError("MOV A C"));
-// console.log(checkInstructionError("MVI A 36"));
-// console.log(checkInstructionError("MVI A AB"));
-// console.log(checkInstructionError("MVI A 3Z"));
-// console.log(checkInstructionError("ADI A 36"));
-// console.log(checkInstructionError("ADI 45"));
-// console.log(checkInstructionError("ADI 4"));
-// console.log(checkInstructionError("STA 4500"));
-// console.log(checkInstructionError("STA 45002"));
-// console.log(checkInstructionError("STA XYZ"));
-// console.log(checkInstructionError("STA LOC"));
-// console.log(checkInstructionError("LXI H 5000"));
-// console.log(parse("LXI H 5000"));
-// console.log(parse("MOV A B"));
-// console.log(parse("MOV A C"));
-// console.log(parse("MVI A 36"));
-// console.log(parse("MVI A AB"));
-// console.log(parse("MVI A 3Z"));
-// console.log(parse("ADI A 36"));
-// console.log(parse("ADI 45"));
-// console.log(parse("ADI 4"));
-// console.log(parse("STA 4500"));
-// console.log(parse("STA 45002"));
-// console.log(parse("STA XYZ"));
-// console.log(parse("STA LOC"));
-// console.log(parse("STAf LOC"));
-
-// label["LOC"] = 5000;
-// console.log(checkInstructionError("LXI H LOC"));
-// console.log(parse("LXI H LOC"));
-// console.log(parse("LXI H 5000"));
-
-// console.log(getInstructions(["MOV","A","B"]));
-// instruction = getInstructions(["MOV","A","B"]);
-// console.log(parse(instruction));
