35cde8bf410e87f5c84a45734db02f5d901acfc1
Sidhant Unnithan <sidhantunnithan@gmail.com>
2021-01-01 21:07:02 +0530

    Added Editor

--- Client/src/Components/Editor.jsx
+++ Client/src/Components/Editor.jsx
@@ -2,10 +2,13 @@ import React, { Component } from "react";
 import "./Styles/BodyStyles.scss";
 
 export class Editor extends Component {
+    constructor(props) {
+        super(props);
+    }
+
     render() {
         return (
             <div className="editor-container">
-                <h1>Editor</h1>
             </div>
         );
     }
