319cd0591bac7be8bd2c436da1ae68317452fc02
Sidhant Unnithan <sidhantunnithan@gmail.com>
2021-01-03 03:00:07 +0530

    Added monaco editor

--- Client/src/Redux/Reducers/rootReducer.js
+++ Client/src/Redux/Reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import registerReducer from "./registerReducer";
 import memoryReducer from "./memoryReducer";
+import editorReducer from "./editorReducer";
 
 export default combineReducers({
     registerReducer,
     memoryReducer,
+    editorReducer,
 });
