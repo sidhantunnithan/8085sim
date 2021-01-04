f342f415626a5f179fd31499d90036d40f4492f4
Sidhant Unnithan <sidhantunnithan@gmail.com>
2021-01-05 01:53:25 +0530

    Added API calls

--- Client/src/Redux/Actions/types.js
+++ Client/src/Redux/Actions/types.js
@@ -1,2 +1,4 @@
 export const EDITOR_CHANGE = "EDITOR_CHANGE"; // When value of editor changes
 export const FILENAME_CHANGE = "FILENAME_CHANGE"; // When file name changes
+export const MEMORY_INIT = "MEMORY_INIT"; // When memory is initialised
+export const MEMORY_RESET = "MEMORY_RESET"; // When memory is reset
