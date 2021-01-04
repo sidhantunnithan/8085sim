1fd9ad6f32a99f894cc9a52fa0e3f91f311bbc73
Sidhant Unnithan <sidhantunnithan@gmail.com>
2021-01-05 01:54:19 +0530

    Added API functionality

--- Server/server.js
+++ Server/server.js
@@ -1,7 +1,34 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const { json } = require("body-parser");
+const { getAssembledInstructions } = require(__dirname + "/assembler.js");
 
 const app = express();
+const jsonParser = bodyParser.json();
+const port = 5000;
 
-app.listen(3000, function(){
+app.get("/", (req, res) => {
+    res.send("<h1> Hello </h1>");
+});
+
+app.post("/api", jsonParser, (req, res) => {
+    if (req.body.hasOwnProperty("code")) {
+        getAssembledInstructions(req.body.code.split(" ")).then(
+            (assembledInstructions) => {
+                res.json({
+                    code: 0,
+                    payload: assembledInstructions,
+                });
+            }
+        );
+    } else {
+        res.json({
+            code: 1,
+            payload: "Invalid Parameters",
+        });
+    }
+});
+
+app.listen(port, function () {
     console.log("server started");
-})
+});
