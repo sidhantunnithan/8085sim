2ba900beb47951b0ddf93b137140244320bbeb19
Sidhant Unnithan <sidhantunnithan@gmail.com>
2021-01-06 06:29:14 +0530

    Removed redux logger junk

--- Client/src/Redux/store.js
+++ Client/src/Redux/store.js
@@ -1,10 +1,9 @@
 import { applyMiddleware, createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
-import logger from "redux-logger";
 import rootReducer from "./Reducers/rootReducer";
 
-const middleware = [logger, thunk];
+const middleware = [thunk];
 
 const initialState = {};
 
